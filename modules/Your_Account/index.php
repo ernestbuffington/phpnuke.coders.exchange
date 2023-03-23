ed9095219151a973320c5abc1b68902d2842d16a
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-23 13:53:04 -0400

    Update index.php

--- modules/Your_Account/index.php
+++ modules/Your_Account/index.php
@@ -900,9 +900,9 @@ function userinfo($username, $bypass=0, $hid=0, $url=0) {
 
 				$row5 = $db->sql_fetchrow($result5);
 
-				$nsitename = filter($row5[sitename], "nohtml");
+				$nsitename = filter($row5['sitename'] ?? '', "nohtml");
 
-				$url = filter($row5[headlinesurl], "nohtml");
+				$url = filter($row5['headlinesurl'], "nohtml");
 
 				$title = filter($nsitename, "nohtml");
 
