a1151418eb69306af90b3651fd27a74bbb5a8d50
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-24 11:42:31 -0400

    Update rector.php

--- rector.php
+++ rector.php
@@ -182,7 +182,9 @@ return static function (RectorConfig $rectorConfig): void {
 		//////__DIR__ . '/admin/modules/blocks.php',
 		//////__DIR__ . '/admin/modules/authors.php',
 		//////__DIR__ . '/admin/modules/groups.php',
-		__DIR__ . '/admin/modules/ipban.php',
+		//////__DIR__ . '/admin/modules/ipban.php',
+		__DIR__ . '/admin/modules/messages.php',
+		
 		//__DIR__ . '/admin/modules/backup/backup.php', # Mod From Titanium #
 		//__DIR__ . '/admin/modules/backup/backupdownload.php',
 		//__DIR__ . '/admin/modules/modules.php',
