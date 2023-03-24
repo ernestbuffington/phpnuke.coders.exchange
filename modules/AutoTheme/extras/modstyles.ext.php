851cd0232ec12eba0ac079cf4d38398f7533a801
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-24 08:55:52 -0400

    Update modstyles.ext.php

--- modules/AutoTheme/extras/modstyles.ext.php
+++ modules/AutoTheme/extras/modstyles.ext.php
@@ -1,50 +1,65 @@
-<?php
-
-// Extra for all platforms
-//
-// How to register an extra and the functions that it performs and when to perform them (at operation)
-//
-// $extra = array ( 'at operation' => 'extra function' );
-//
-$extra['modstyles'] = array (
-	'name' => 'Module Styles',
-	'description' => 'Add style definitions for module content',
-	'version' => '1.7',
-	'author' => 'Shawn McKenzie',
-	'contact' => 'http://spidean@mckenzies.net',
-	'modopen' => 'open_mod_styles',
-	'modclose' => 'close_mod_styles',
-	'atadmin' => ''
-);
-
-// Extra functions
-//
-function open_mod_styles($vars)
-{
-	extract($vars);
-
-    $modname = atGetModName();
-    
-    if (!isset($tag)) {
-    	$tag = "div";
-    }
-	$display .= "\n<!-- Open Module Styles -->\n"
-	."<$tag id=\"$modname\" class=\"module\">\n\n";
-	
-    echo $display;
-}
-
-function close_mod_styles($vars)
-{
-	extract($vars);
-	
-	if (!isset($tag)) {
-    	$tag = "div";
-    }
-    $display .= "\n<!-- Close Module Styles -->\n"
-	."</$tag>\n\n";
-
-  	echo $display;
-}
-
-?>
+<?php
+
+/* Applied rules:
+ * AddDefaultValueForUndefinedVariableRector (https://github.com/vimeo/psalm/blob/29b70442b11e3e66113935a2ee22e165a70c74a4/docs/fixing_code.md#possiblyundefinedvariable)
+ */
+
+// Extra for all platforms
+//
+// How to register an extra and the functions that it performs and when to perform them (at operation)
+//
+// $extra = array ( 'at operation' => 'extra function' );
+//
+$extra['modstyles'] = array (
+	'name' => 'Module Styles',
+	'description' => 'Add style definitions for module content',
+	'version' => '1.7',
+	'author' => 'Shawn McKenzie',
+	'contact' => 'http://spidean@mckenzies.net',
+	'modopen' => 'open_mod_styles',
+	'modclose' => 'close_mod_styles',
+	'atadmin' => ''
+);
+
+// Extra functions
+//
+function open_mod_styles($vars)
+{
+	$display = null;
+    
+	extract($vars);
+
+    $modname = atGetModName();
+
+    if (!isset($tag)) {
+
+    	$tag = "div";
+    }
+
+	$display .= "\n<!-- Open Module Styles -->\n"
+
+	."<$tag id=\"$modname\" class=\"module\">\n\n";
+
+    echo $display;
+}
+
+function close_mod_styles($vars)
+{
+	$display = null;
+    
+	extract($vars);
+
+	if (!isset($tag)) {
+
+    	$tag = "div";
+    }
+
+    $display .= "\n<!-- Close Module Styles -->\n"
+
+	."</$tag>\n\n";
+
+  	echo $display;
+}
+
+?>
+
