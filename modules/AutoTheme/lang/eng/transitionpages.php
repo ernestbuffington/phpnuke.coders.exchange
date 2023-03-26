ee2c2ecd43a67e83c2425912b3f3c12c249940da
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-26 08:22:14 -0400

    Update

--- modules/AutoTheme/lang/eng/transitionpages.php
+++ modules/AutoTheme/lang/eng/transitionpages.php
@@ -1,9 +1,9 @@
-<?php
-
-define('_AT_TEMPLATE', 'Template');
-
-define('_AT_ROTATE', 'Rotate through multiple templates above?');
-define('_AT_VISITS', 'Number of visits before page is displayed?');
-
-
-?>
+<?php
+
+define('_AT_TEMPLATE', 'Template');
+
+define('_AT_ROTATE', 'Rotate through multiple templates above?');
+define('_AT_VISITS', 'Number of visits before page is displayed?');
+
+
+?>
