e8428fa8807153dc0c6e2e4f3c66adb8cd023a93
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-29 18:43:01 -0400

    Update header.php

--- header.php
+++ header.php
@@ -41,18 +41,17 @@ function head() {
 		$pagetitle, $cache, $ThemeSel;
 
 	$ThemeSel = get_theme();
-	include_secure("themes/$ThemeSel/theme.php");
 
-  /**
-   * Doctype/Mime Type auto selector - This checks each theme as it is switched and will load a mimetype.php from the themes includes folder.
-   * This allows for many different doctypes to be used on the Fly by whichever theme is selected.
-   * This is great for porting Legacy themes or even just to show the versatility of PHP-Nuke.
-   * If a mimetype.php file is not detected it uses the default doctype of XHTML 1.0 Transitional
-   *
-   * @author Ernest Allen Bufffington
-   * @version 1.0
-   * @license GPL-3.0
-   */
+   /**
+    * Doctype/Mime Type auto selector - This checks each theme as it is switched and will load a mimetype.php from the themes includes folder.
+    * This allows for many different doctypes to be used on the Fly by whichever theme is selected.
+    * This is great for porting Legacy themes or even just to show the versatility of PHP-Nuke.
+    * If a mimetype.php file is not detected it uses the default doctype of XHTML 1.0 Transitional
+    *
+    * @author Ernest Allen Bufffington
+    * @version 1.0
+    * @license GPL-3.0
+    */
   	if (file_exists(NUKE_THEMES_DIR.$ThemeSel.'/includes/mimetype.php')):  
     include(NUKE_THEMES_DIR.$ThemeSel.'/includes/mimetype.php');
 	echo "<!-- HEADER START ================================================================================================================================================================================================= -->\n";
@@ -169,7 +168,10 @@ function head() {
         
 		endif;
     endif;
-    
+
+	echo "\n<!-- Loadiing themes/".$ThemeSel."/theme.php from header.php -->\n";
+    include_once(NUKE_THEMES_DIR.$ThemeSel.'/theme.php');
+	    
    /*
     * functions added to support dynamic and ordered loading of CSS, PHPCSS, and JS in <head> and before </body>
     * Code origin Raven Nuke CMS (http://www.ravenphpscripts.com)
