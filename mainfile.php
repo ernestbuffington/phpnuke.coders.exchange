1d0424d5e627137a41db17391534e6761af77898
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-01 10:47:53 -0400

    Update mainfile.php

--- mainfile.php
+++ mainfile.php
@@ -280,6 +280,7 @@ define('NUKE_CSS_DIR', 'includes/css/');
 define('NUKE_IMAGES_DIR', NUKE_BASE_DIR . 'images/');
 define('NUKE_INCLUDE_DIR', NUKE_BASE_DIR . 'includes/');
 define('PHPBB3_INCLUDE_DIR', NUKE_BASE_DIR . 'includes/mods/phpbb3/');
+define('PHPBB3_ADMIN_DIR', NUKE_BASE_DIR . 'modules/phpBB3/adm/');
 define('NUKE_JQUERY_INCLUDE_DIR', 'includes/js/');
 define('NUKE_JQUERY_SCRIPTS_DIR', 'includes/js/scripts/');
 define('NUKE_LANGUAGE_DIR', NUKE_BASE_DIR . 'language/');
