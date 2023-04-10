efc497a5928cfccdad7c9d47258c84c7bde25aa8
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-10 07:39:03 -0400

    Update functions.php

--- modules/Your_Account/includes/functions.php
+++ modules/Your_Account/includes/functions.php
@@ -12,27 +12,27 @@
 /************************************************************************/
 
 /*********************************************************************************/
-/* CNB Your Account: An Advanced User Management System for phpnuke             */
-/* ============================================                                 */
-/*                                                                              */
-/* Copyright (c) 2004 by Comunidade PHP Nuke Brasil                             */
-/* http://dev.phpnuke.org.br & http://www.phpnuke.org.br                        */
-/*                                                                              */
-/* Contact author: escudero@phpnuke.org.br                                      */
-/* International Support Forum: http://ravenphpscripts.com/forum76.html         */
-/*                                                                              */
-/* This program is free software. You can redistribute it and/or modify         */
-/* it under the terms of the GNU General Public License as published by         */
-/* the Free Software Foundation; either version 2 of the License.               */
-/*                                                                              */
+/* CNB Your Account: An Advanced User Management System for phpnuke              */
+/* ============================================                                  */
+/*                                                                               */
+/* Copyright (c) 2004 by Comunidade PHP Nuke Brasil                              */
+/* http://dev.phpnuke.org.br & http://www.phpnuke.org.br                         */
+/*                                                                               */
+/* Contact author: escudero@phpnuke.org.br                                       */
+/* International Support Forum: http://ravenphpscripts.com/forum76.html          */
+/*                                                                               */
+/* This program is free software. You can redistribute it and/or modify          */
+/* it under the terms of the GNU General Public License as published by          */
+/* the Free Software Foundation; either version 2 of the License.                */
+/*                                                                               */
 /*********************************************************************************/
-/* CNB Your Account it the official successor of NSN Your Account by Bob Marion    */
+/* CNB Your Account is the official successor of NSN Your Account by Bob Marion  */
 /*********************************************************************************/
 
 /*****['CHANGES']**********************************************************
 -=['Base']=-
-      Nuke Patched                             v3.1.0       06/26/2005
-      NukeSentinel                             v2.5.00      07/11/2006
+      PHP Patched                              v8.2.4       04/10/2023
+      NukeSentinel                             v2.6.16      07/11/2006
       Caching System                           v1.0.0       10/31/2005
       Evolution Functions                      v1.5.0       12/20/2005
  ************************************************************************/
