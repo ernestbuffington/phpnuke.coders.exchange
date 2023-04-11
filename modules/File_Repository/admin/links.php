a2d3d3ea35790113bc93c5313f8613eda2967c54
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-11 02:02:34 -0400

    Update links.php

--- modules/File_Repository/admin/links.php
+++ modules/File_Repository/admin/links.php
@@ -15,17 +15,19 @@
 /*
 |-----------------------------------------------------------------------
 |	COPYRIGHT (c) 2016 by lonestar-modules.com
-|	AUTHOR 				:	Lonestar	
+|	AUTHOR(s) 			:	Lonestar, Ernest Allen Buffington	
 |	COPYRIGHTS 			:	lonestar-modules.com
 |	PROJECT 			:	File Repository
-|	VERSION 			:	1.0.0
+|	VERSION 			:	1.1.1
 |----------------------------------------------------------------------
 */
 
-if (!defined('ADMIN_FILE')) 
+if (!defined('ADMIN_FILE')) {
    exit('THIS FILE WAS NOT CALLED WITHIN ADMINISTRATION');
+}
 
 global $admin_file, $module_title;
+
 adminmenu($admin_file.'.php?op=file_repository', 'File Repository', 'file_repository.png');
 
 ?>
