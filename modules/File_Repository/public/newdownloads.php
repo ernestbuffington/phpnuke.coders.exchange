4bfc7aa3ff816e68bc41f89c4a0d23509a6dbd39
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-11 02:35:44 -0400

    Update newdownloads.php

--- modules/File_Repository/public/newdownloads.php
+++ modules/File_Repository/public/newdownloads.php
@@ -30,6 +30,7 @@ function _show_list_newdownloads()
 	
 	$counter = 0;
 	$allweekdownloads = 0;
+	
 	while ($counter <= 7-1)
 	{
 		$newdownloaddayRaw = (time()-(86400 * $counter));
@@ -41,6 +42,7 @@ function _show_list_newdownloads()
 	
 	$counter = 0;
 	$allmonthdownloads = 0;
+	
 	while ($counter <=30-1)
 	{
 		$newdownloaddayRaw = (time()-(86400 * $counter));
@@ -56,19 +58,25 @@ function _show_list_newdownloads()
 	_index_navigation_header();	
 	echo '<br />';
 	echo '<table width="100%" border="0" cellpadding="4" cellspacing="1" class="forumline">'."\n";
-	echo '	<tr'._bgColor(2).'>'."\n";
-	echo '	  <td'._tdcss(false,'center',_sh(),2).'>'._suh($lang_new[$module_name]['NEW_DOWNLOADS']).'</td>'."\n";
-	echo '  </tr>'."\n";
-	echo '	<tr'._bgColor(1).'>'."\n";
-	echo '	  <td'._tdcss(false,'center',_sc(),2).'>'._sut($lang_new[$module_name]['NEW_DOWNLOADS_TOTALS']).': '._sut($lang_new[$module_name]['LASTWEEK']).' - '.$allweekdownloads.' \ '._sut($lang_new[$module_name]['LAST30']).' - '.$allmonthdownloads.'</td>'."\n";
-	echo '  </tr>'."\n";
-	echo '	<tr'._bgColor(2).'>'."\n";
-	echo '	  <td'._tdcss(false,'center',_sh(),2).'><a'._ls().' href="modules.php?name='.$module_name.'&amp;action=newdownloads&amp;newdownloadshowdays=7">'._suh($lang_new[$module_name]['1WEEK']).'</a> - <a'._ls().' href="modules.php?name='.$module_name.'&amp;action=newdownloads&amp;newdownloadshowdays=14">'._suh($lang_new[$module_name]['2WEEKS']).'</a> - <a'._ls().' href="modules.php?name='.$module_name.'&amp;action=newdownloads&amp;newdownloadshowdays=30">'._suh($lang_new[$module_name]['30DAYS']).'</a></td>'."\n";
-	echo '  </tr>'."\n";		
-	echo '	<tr'._bgColor(1).'>'."\n";
-	echo '	  <td'._tdcss(false,'center',_sc(),2).'>'._sut(sprintf($lang_new[$module_name]['NEW_DOWNLOADS_LAST'],$newdownloadshowdays)).':<br /><br />';		
+	echo '<tr'._bgColor(2).'>'."\n";
+	echo '<td'._tdcss(false,'center',_sh(),2).'>'._suh($lang_new[$module_name]['NEW_DOWNLOADS']).'</td>'."\n";
+	echo '</tr>'."\n";
+	echo '<tr'._bgColor(1).'>'."\n";
+	
+	echo '<td'._tdcss(false,'center',_sc(),2).'>'._sut($lang_new[$module_name]['NEW_DOWNLOADS_TOTALS']).': '._sut($lang_new[$module_name]['LASTWEEK']).' - '.$allweekdownloads.' \ '._sut($lang_new[$module_name]['LAST30']).' - '.$allmonthdownloads.'</td>'."\n";
+	
+	echo '</tr>'."\n";
+	echo '<tr'._bgColor(2).'>'."\n";
+	
+	echo '<td'._tdcss(false,'center',_sh(),2).'><a'._ls().' href="modules.php?name='.$module_name.'&amp;action=newdownloads&amp;newdownloadshowdays=7">'._suh($lang_new[$module_name]['1WEEK']).'</a> - <a'._ls().' href="modules.php?name='.$module_name.'&amp;action=newdownloads&amp;newdownloadshowdays=14">'._suh($lang_new[$module_name]['2WEEKS']).'</a> - <a'._ls().' href="modules.php?name='.$module_name.'&amp;action=newdownloads&amp;newdownloadshowdays=30">'._suh($lang_new[$module_name]['30DAYS']).'</a></td>'."\n";
+	
+	echo '</tr>'."\n";		
+	echo '<tr'._bgColor(1).'>'."\n";
+	echo '<td'._tdcss(false,'center',_sc(),2).'>'._sut(sprintf($lang_new[$module_name]['NEW_DOWNLOADS_LAST'],$newdownloadshowdays)).':<br /><br />';		
+	
 	$counter = 0;
 	$allweekdownloads = 0;
+	
 	while ($counter <= $newdownloadshowdays-1) 
 	{
 		$newdownloaddayRaw = (time()-(86400 * $counter));
@@ -78,13 +86,15 @@ function _show_list_newdownloads()
 		$allweekdownloads = $allweekdownloads + $totaldownloads;
 		echo '<strong><big>&middot;</big></strong> <a href="modules.php?name='.$module_name.'&amp;action=newdownloadsdate&amp;selectdate='.$newdownloaddayRaw.'">'.$newdownloadDB.'</a>&nbsp('.$totaldownloads.')<br />'."\n";
 	}
+	
 	$counter = 0;
 	$allmonthdownloads = 0;		
+	
 	echo '</td>'."\n";
-	echo '  </tr>'."\n";		
-	echo '	<tr'._bgColor(2).'>'."\n";
-	echo '	  <td'._tdcss(false,'center',_sf(),2).'>&nbsp;</td>'."\n";
-	echo '  </tr>'."\n";		
+	echo '</tr>'."\n";		
+	echo '<tr'._bgColor(2).'>'."\n";
+	echo '<td'._tdcss(false,'center',_sf(),2).'>&nbsp;</td>'."\n";
+	echo '</tr>'."\n";		
 	echo '</table>'."\n";	
 	CloseTable();	
 }
@@ -103,44 +113,54 @@ function _show_list_newdownloads_dates()
 	$sql 		= "SELECT * FROM `"._FILE_REPOSITORY_ITEMS."` WHERE `date` LIKE '%".$newdownloadDB."%' AND `isactive` > '0'";
 	$result  	= $db->sql_query($sql);
 	$numrows 	= $db->sql_numrows($result);
+	
 	echo '<br />';
 	echo '<table width="100%" border="0" cellpadding="4" cellspacing="1" class="forumline">'."\n";
-	echo '  <tr'._bgColor(2).'>'."\n";
-	echo '	  <td'._tdcss(false,'center',_sh(),(($settings['download_view'] == 0) ? '6' : $themes[get_theme()]['per_row'])).'>'._timestamp($dateView,'M jS, Y').' - '._suh($lang_new[$module_name]['NEW_DOWNLOADS']).'</td>'."\n";
-	echo '  </tr>'."\n";
+	echo '<tr'._bgColor(2).'>'."\n";
+	echo '<td'._tdcss(false,'center',_sh(),(($settings['download_view'] == 0) ? '6' : $themes[get_theme()]['per_row'])).'>'._timestamp($dateView,'M jS, Y').' - '._suh($lang_new[$module_name]['NEW_DOWNLOADS']).'</td>'."\n";
+	echo '</tr>'."\n";
+	
 	if($numrows > 0)
 	{
 		if($settings['download_view'] == 0)
 		{
-			echo '  <tr'._bgColor(2).'>'."\n";
-			echo '    <td'._tdcss(false,false,_sh(),2).'>'._suh($lang_new[$module_name]['TITLE']).'</td>'.PHP_EOL;
-			echo '    <td'._tdcss('10%','center',_sh()).'>'._suh($lang_new[$module_name]['VIEWS']).'</td>'.PHP_EOL;
-			echo '    <td'._tdcss('10%','center',_sh()).'>'._suh($lang_new[$module_name]['DOWNLOADS']).'</td>'.PHP_EOL;
-			echo '    <td'._tdcss('10%','center',_sh()).'>'._suh($lang_new[$module_name]['FILE_SIZE']).'</td>'.PHP_EOL;
-			echo '    <td'._tdcss('20%','center',_sh()).'>'._suh($lang_new[$module_name]['DATE_ADDED']).'</td>'.PHP_EOL;
-			echo '  </tr>'."\n";
+			echo '<tr'._bgColor(2).'>'."\n";
+			echo '<td'._tdcss(false,false,_sh(),2).'>'._suh($lang_new[$module_name]['TITLE']).'</td>'.PHP_EOL;
+			echo '<td'._tdcss('10%','center',_sh()).'>'._suh($lang_new[$module_name]['VIEWS']).'</td>'.PHP_EOL;
+			echo '<td'._tdcss('10%','center',_sh()).'>'._suh($lang_new[$module_name]['DOWNLOADS']).'</td>'.PHP_EOL;
+			echo '<td'._tdcss('10%','center',_sh()).'>'._suh($lang_new[$module_name]['FILE_SIZE']).'</td>'.PHP_EOL;
+			echo '<td'._tdcss('20%','center',_sh()).'>'._suh($lang_new[$module_name]['DATE_ADDED']).'</td>'.PHP_EOL;
+			echo '</tr>'."\n";
+			
 			while($popular = $db->sql_fetchrow($result))
 			{
 				$v 			= (($popular['version']) ? sprintf($lang_new[$module_name]['V'],$popular['version']) : '');
 				$iteminfo 	= _collect_iteminfo($popular['did']);
 				$screen[$items] = $db->sql_fetchrow($db->sql_query("SELECT `filename`, `title` FROM `"._FILE_REPOSITORY_SCREENSHOTS."` WHERE `did`='".$popular['did']."' ORDER BY RAND()"));
 				echo '  <tr'._bgColor(1).'>'."\n";
+			
 				if($screen[$items]['filename'])
 				{
-					echo '    <td'._tdcss('5%','center',_sc()).'><a'._image_viewer($screen[$items]['filename'],'item-'.$popular['did']).' href="'._FILE_REPOSITORY_SCREENS.$screen[$items]['filename'].'" title="'.$screen[$items]['title'].'"><img style="border: 1px solid white; height: 28px; width: 30px;" src="modules/'.$module_name.'/files/screenshots/'.$screen[$items]['filename'].'" border="0" /></a></td>';
-					echo '    <td'._tdcss('45%',false,_sc()).'>';
-					echo '      <a'._ls().' href="modules.php?name='.$module_name.'&amp;action=view&amp;did='.$popular['did'].'">'._colorization($iteminfo['title'],$iteminfo['color']).$v.'</a>'._item_is_new($iteminfo['isnew'],$iteminfo['updated'])._mostpopular($iteminfo['hits']);
-					echo '    </td>';
+					echo '<td'._tdcss('5%','center',_sc()).'><a'._image_viewer($screen[$items]['filename'],'item-'.$popular['did']).' href="'._FILE_REPOSITORY_SCREENS.$screen[$items]['filename'].'" title="'.$screen[$items]['title'].'"><img style="border: 1px solid white; height: 28px; width: 30px;" src="modules/'.$module_name.'/files/screenshots/'.$screen[$items]['filename'].'" border="0" /></a></td>';
+					
+					echo '<td'._tdcss('45%',false,_sc()).'>';
+					
+					echo '<a'._ls().' href="modules.php?name='.$module_name.'&amp;action=view&amp;did='.$popular['did'].'">'._colorization($iteminfo['title'],$iteminfo['color']).$v.'</a>'._item_is_new($iteminfo['isnew'],$iteminfo['updated'])._mostpopular($iteminfo['hits']);
+					
+					echo '</td>';
 				} else {
-					echo '    <td'._tdcss(false,false,_sc(),2).'>';
-					echo '      <a'._ls().' href="modules.php?name='.$module_name.'&amp;action=view&amp;did='.$popular['did'].'">'._colorization($iteminfo['title'],$iteminfo['color']).$v.'</a>'._item_is_new($iteminfo['isnew'],$iteminfo['updated'])._mostpopular($iteminfo['hits']);
-					echo '    </td>';
+					echo '<td'._tdcss(false,false,_sc(),2).'>';
+					
+					echo '<a'._ls().' href="modules.php?name='.$module_name.'&amp;action=view&amp;did='.$popular['did'].'">'._colorization($iteminfo['title'],$iteminfo['color']).$v.'</a>'._item_is_new($iteminfo['isnew'],$iteminfo['updated'])._mostpopular($iteminfo['hits']);
+					
+					echo '</td>';
 				}
-				echo '    <td'._tdcss('10%','center',_sc()).'>'.$iteminfo['views'].'</td>';
-				echo '    <td'._tdcss('10%','center',_sc()).'>'.$iteminfo['hits'].'</td>';
-				echo '    <td'._tdcss('10%','center',_sc()).'>'._sut($iteminfo['filesize']).'</td>';
-				echo '    <td'._tdcss('20%','center',_sc()).'>'.$iteminfo['date'].'</td>';
-				echo '  </tr>'."\n";
+				echo '<td'._tdcss('10%','center',_sc()).'>'.$iteminfo['views'].'</td>';
+				echo '<td'._tdcss('10%','center',_sc()).'>'.$iteminfo['hits'].'</td>';
+				echo '<td'._tdcss('10%','center',_sc()).'>'._sut($iteminfo['filesize']).'</td>';
+				echo '<td'._tdcss('20%','center',_sc()).'>'.$iteminfo['date'].'</td>';
+				echo '</tr>'."\n";
+				
 				$items++;
 			}
 			$db->sql_freeresult($result);
@@ -154,6 +174,7 @@ function _show_list_newdownloads_dates()
 				$iteminfo 		= _collect_iteminfo($d['did']);
 				$screen[$items] = $db->sql_fetchrow($db->sql_query("SELECT `filename`, `title` FROM `"._FILE_REPOSITORY_SCREENSHOTS."` WHERE `did`='".$d['did']."' ORDER BY RAND()"));
 				$ustring 		= ($iteminfo['updated'] == '0000-00-00 00:00:00') ? _sut($lang_new[$module_name]['DATE_ADDED']) : _sut($lang_new[$module_name]['UPDATED']);
+	
 				if($screen[$items]['filename'])
 					$colspan = false;
 				else 
@@ -169,36 +190,42 @@ function _show_list_newdownloads_dates()
 				if ($items == 0)
 					echo '  <tr'._bgColor(1).'>'."\n";
 
-				echo '    <td'._tdcss($filePerRow,false,_sc()).' valign="top">'."\n";
-				echo '      <table width="100%" border="0" cellpadding="4" cellspacing="1" class="forumline">'."\n";
-				echo '        <tr'._bgColor(1).'>'."\n";
+				echo '<td'._tdcss($filePerRow,false,_sc()).' valign="top">'."\n";
+				echo '<table width="100%" border="0" cellpadding="4" cellspacing="1" class="forumline">'."\n";
+				echo '<tr'._bgColor(1).'>'."\n";
 				if($screen[$items]['filename'])
-				echo '          <td'._tdcss('10%','center',_sc()).'><div class="thumbnail_border"><a'._image_viewer($screen[$items]['filename'],'item-'.$d['did']).' href="'._FILE_REPOSITORY_SCREENS.$screen[$items]['filename'].'" title="'.$screen[$items]['title'].'"><img src="'._FILE_REPOSITORY_SCREENS.'thumbs/thumb_100x100_'.$screen[$items]['filename'].'" border="0" /></a></div></td>';
-				echo '          <td'._tdcss('90%',false,_sc(),$colspan).' valign="top">';
-				echo '            <table width="100%" border="0" cellpadding="4" cellspacing="1">'."\n";
-				echo '              <tr>'."\n";
-				echo '                <td'._tdcss(false,false,false,false,true).'>';
-				echo '                  '._sut($lang_new[$module_name]['DOWNLOAD']).' : <a'._ls().' href="modules.php?name='.$module_name.'&amp;action=view&amp;did='.$d['did'].'">'._colorization($iteminfo['title'],$iteminfo['color']).'</a>'._item_is_new($iteminfo['isnew'],$iteminfo['updated'])._mostpopular($iteminfo['hits']);
-				echo '                </td>'."\n";
-				echo '              </tr>'."\n";
-				echo '              <tr>'."\n";
-				echo '                <td'._tdcss(false,false,false,false,true).'>'._sut($lang_new[$module_name]['PERMISSIONS']).' : '.$iteminfo['whocan'].'</td>';
-				echo '              </tr>'."\n";
-				echo '              <tr>'."\n";
-				echo '                <td'._tdcss(false,false,false,false,true).'>'._sut($lang_new[$module_name]['FILE_VERSION']).' : '.(($iteminfo['version']) ? $iteminfo['version'] : $lang_new[$module_name]['NA']).'</td>';
-				echo '              </tr>'."\n";
-				echo '              <tr>'."\n";
-				echo '                <td'._tdcss(false,false,false,false,true).'>'.$ustring.' : '.$iteminfo['isupdated'].'</td>';
-				echo '              </tr>'."\n";
-				echo '              <tr>'."\n";
-				echo '                <td'._tdcss(false,false,false,false,true).'>'._sut($lang_new[$module_name]['DOWNLOADS']).' : '.$iteminfo['hits'].'</td>';
-				echo '              </tr>'."\n";
-				echo '            </table>'."\n";
-				echo '          </td>';
-				echo '        </tr>';
-				echo '      </table>'."\n";
-				echo '    </td>';
+				
+				echo '<td'._tdcss('10%','center',_sc()).'><div class="thumbnail_border"><a'._image_viewer($screen[$items]['filename'],'item-'.$d['did']).' 
+				href="'._FILE_REPOSITORY_SCREENS.$screen[$items]['filename'].'" title="'.$screen[$items]['title'].'"><img 
+				src="'._FILE_REPOSITORY_SCREENS.'thumbs/thumb_100x100_'.$screen[$items]['filename'].'" border="0" /></a></div></td>';
+				
+				echo '<td'._tdcss('90%',false,_sc(),$colspan).' valign="top">';
+				echo '<table width="100%" border="0" cellpadding="4" cellspacing="1">'."\n";
+				echo '<tr>'."\n";
+				echo '<td'._tdcss(false,false,false,false,true).'>';
+				echo ''._sut($lang_new[$module_name]['DOWNLOAD']).' : <a'._ls().' href="modules.php?name='.$module_name.'&amp;action=view&amp;did='.$d['did'].'">'._colorization($iteminfo['title'],$iteminfo['color']).'</a>'._item_is_new($iteminfo['isnew'],$iteminfo['updated'])._mostpopular($iteminfo['hits']);
+				echo '</td>'."\n";
+				echo '</tr>'."\n";
+				echo '<tr>'."\n";
+				echo '<td'._tdcss(false,false,false,false,true).'>'._sut($lang_new[$module_name]['PERMISSIONS']).' : '.$iteminfo['whocan'].'</td>';
+				echo '</tr>'."\n";
+				echo '<tr>'."\n";
+				echo '<td'._tdcss(false,false,false,false,true).'>'._sut($lang_new[$module_name]['FILE_VERSION']).' : '.(($iteminfo['version']) ? $iteminfo['version'] : $lang_new[$module_name]['NA']).'</td>';
+				echo '</tr>'."\n";
+				echo '<tr>'."\n";
+				echo '<td'._tdcss(false,false,false,false,true).'>'.$ustring.' : '.$iteminfo['isupdated'].'</td>';
+				echo '</tr>'."\n";
+				echo '<tr>'."\n";
+				echo '<td'._tdcss(false,false,false,false,true).'>'._sut($lang_new[$module_name]['DOWNLOADS']).' : '.$iteminfo['hits'].'</td>';
+				echo '</tr>'."\n";
+				echo '</table>'."\n";
+				echo '</td>';
+				echo '</tr>';
+				echo '</table>'."\n";
+				echo '</td>';
+				
 				$items++;
+				
 				if ($items == $themes[get_theme()]['per_row']) 
 				{ 
 					echo '  </tr>'."\n";
@@ -207,18 +234,18 @@ function _show_list_newdownloads_dates()
 			}
 			if ($items == 1) 
 			{
-    			echo '    <td'._tdcss($filePerRow,false,_sc()).'>&nbsp;</td>'."\n";
-    			echo '  </tr>'."\n";
+    			echo '<td'._tdcss($filePerRow,false,_sc()).'>&nbsp;</td>'."\n";
+    			echo '</tr>'."\n";
   			}
 		}
 	} else {
-		echo '  <tr'._bgColor(1).'>'."\n";
-		echo '	  <td'._tdcss(false,'center',_sc(),(($settings['download_view'] == 0) ? '6' : $themes[get_theme()]['per_row'])).'>'._sut($lang_new[$module_name]['NOINFO']).'</td>'."\n";
-		echo '  </tr>'."\n";
+		echo '<tr'._bgColor(1).'>'."\n";
+		echo '<td'._tdcss(false,'center',_sc(),(($settings['download_view'] == 0) ? '6' : $themes[get_theme()]['per_row'])).'>'._sut($lang_new[$module_name]['NOINFO']).'</td>'."\n";
+		echo '</tr>'."\n";
 	}
-	echo '  <tr'._bgColor(2).'>'."\n";
-	echo '    <td'._tdcss(false,false,_sf(),(($settings['download_view'] == 0) ? '6' : $themes[get_theme()]['per_row'])).'>&nbsp;</td>'."\n";
-	echo '  </tr>'."\n";
+	echo '<tr'._bgColor(2).'>'."\n";
+	echo '<td'._tdcss(false,false,_sf(),(($settings['download_view'] == 0) ? '6' : $themes[get_theme()]['per_row'])).'>&nbsp;</td>'."\n";
+	echo '</tr>'."\n";
 	echo '</table>'."\n";
 	CloseTable();
 }
