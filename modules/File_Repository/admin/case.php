784f1e38f14ea846ab5b5629c85dff5d1ba7e768
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-11 01:59:07 -0400

    Update case.php

--- modules/File_Repository/admin/case.php
+++ modules/File_Repository/admin/case.php
@@ -15,27 +15,23 @@
 /*
 |-----------------------------------------------------------------------
 |	COPYRIGHT (c) 2016 by lonestar-modules.com
-|	AUTHOR 				:	Lonestar	
+|	AUTHOR(s) 			:	Lonestar, Ernest Allen Buffington	
 |	COPYRIGHTS 			:	lonestar-modules.com
 |	PROJECT 			:	File Repository
-|	VERSION 			:	1.0.0
+|	VERSION 			:	1.1.1
 |----------------------------------------------------------------------
 */
 
 $module_name = basename(dirname(dirname(__FILE__)));
-//-------------------------------------------------------------------------
+
 //  INCLUDE THE LANGUAGE FILE FOR THE MODULE.
-//-------------------------------------------------------------------------
 include_once(NUKE_MODULES_DIR.$module_name.'/language/lang-english.php');
-//-------------------------------------------------------------------------
-//  INCLUDE THE LANGUAGE FILE FOR THE MODULE.
-//-------------------------------------------------------------------------
+
 switch($op) 
 {
     case 'file_repository':
     	include(NUKE_MODULES_DIR.$module_name.'/admin/index.php');
     	break;
-
 }
 
 ?>
