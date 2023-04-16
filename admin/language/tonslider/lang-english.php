3429582207ee8bc46e05efe10146ebb37ac8de1e
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-16 03:17:51 -0400

    Create lang-english.php

---
+++ admin/language/tonslider/lang-english.php
@@ -0,0 +1,47 @@
+<?php
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+define_once("_BOOTSTRAP_SLIDER","Bootstrap Slider");
+
+define("_TONSLIDERCONFIG","Tricked Out News Slider Control Panel");
+define("_TONSLIDERSETUP","Control the features of Tricked Out News SLider");
+define("_SLIDERWIDTH","<b>Width of Slider</b>");
+define("_SLIDERHEIGHT","<b>Height of Slider</b>");
+define("_SLIDERDURATION","<b>Duration of Slider</b>");
+define("_SLIDER1TITLE","<b>Slide 1 Title</b>");
+define("_SLIDER2TITLE","<b>Slide 2 Title</b>");
+define("_SLIDER3TITLE","<b>Slide 3 Title</b>");
+define("_SLIDER4TITLE","<b>Slide 4 Title</b>");
+define("_SLIDER5TITLE","<b>Slide 5 Title</b>");
+define("_SLIDER6TITLE","<b>Slide 6 Title</b>");
+define("_SLIDER1","<b>Slide 1 Content</b>");
+define("_SLIDER2","<b>Slide 2 Content</b>");
+define("_SLIDER3","<b>Slide 3 Content</b>");
+define("_SLIDER4","<b>Slide 4 Content</b>");
+define("_SLIDER5","<b>Slide 5 Content</b>");
+define("_SLIDER6","<b>Slide 6 Content</b>");
+define("_SLIDERTHUMB1","<b>Slide 1 Thumb</b>");
+define("_SLIDERTHUMB2","<b>Slide 2 Thumb</b>");
+define("_SLIDERTHUMB3","<b>Slide 3 Thumb</b>");
+define("_SLIDERTHUMB4","<b>Slide 4 Thumb</b>");
+define("_SLIDERTHUMB5","<b>Slide 5 Thumb</b>");
+define("_SLIDERTHUMB6","<b>Slide 6 Thumb</b>");
+
+
+?>
